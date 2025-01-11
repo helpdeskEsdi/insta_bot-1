1eba8373595aa68a08c426881d5852ef0f048100
xavifarmar <xavierfarmar@campus.monlau.com>
2025-01-11 13:47:11 +0100

    Neteja arxius

--- bot/telegram_utils.py
+++ bot/telegram_utils.py
@@ -4,5 +4,5 @@ def send_profile_image(token, bot_chatID, screenshot_path):
     url = f'https://api.telegram.org/bot{token}/sendPhoto'
     
     with open(screenshot_path, 'rb') as photo:
-        response = requests.post(url, data={'chat_id': bot_chatID}, files={'photo': photo})
+        response = requests.post(url, data={'chat_id': bot_chatID}, message={'text:'"Foto del perfil"}, files={'photo': photo})
     return response
